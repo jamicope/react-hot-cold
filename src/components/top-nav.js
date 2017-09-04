637ac99788e0fed91fe34cdb0c412e52ba2c03a9
Jami Cope <jamicope@gmail.com>
2017-09-04 17:51:14 -0600

    adding redux actions, reducers, store

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,33 +1,33 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import {newGame, toggleInfoModal} from '../actions';
 
 import './top-nav.css';
 
-export default class TopNav extends React.Component {
-    onNewGame(event) {
+export class TopNav extends React.Component {
+    newGame(event) {
         event.preventDefault();
-        if (this.props.onNewGame) {
-            this.props.onNewGame();
+            this.props.dispatch(newGame());
         }
+
+    toggleInfoModal(event) {
+        event.preventDefault();
+        this.props.dispatch(toggleInfoModal());
     }
 
-    onInfo(event) {
-        event.preventDefault();
-        if (this.props.onInfo) {
-            this.props.onInfo();
-        }
-    }
 
     render() {
         return (
             <nav>
                 <ul className="clearfix">
                     <li>
-                        <a className="what" href="#" onClick={e => this.onInfo(e)}>
+                        <a className="what" href="#" onClick={e => this.toggleInfoModal(e)}>
                             What?
                         </a>
                     </li>
                     <li>
-                        <a className="new" href="#" onClick={e => this.onNewGame(e)}>
+                        <a className="new" href="#" onClick={e => this.newGame(e)}>
                             + New Game
                         </a>
                     </li>
@@ -37,3 +37,4 @@ export default class TopNav extends React.Component {
     }
 };
 
+export default connect()(TopNav);
---
+++ src/store.js
@@ -0,0 +1,3 @@
+import {createStore, applyMiddleware} from 'redux';
+import reducer from './reducer';
+export default createStore(reducer);
